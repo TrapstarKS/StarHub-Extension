419db6b7f6e586cd55aea4b2e7b50b810d5ff6d9
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2024-05-15 18:28:13 -0300

    Fixed outputs again

--- src/wsConnection.lua
+++ src/wsConnection.lua
@@ -14,15 +14,14 @@ while task.wait(1) do
 		connection:Send(Encode({ type = "auth", name = game.Players.LocalPlayer.Name }))
 		warn("Sent auth request to " .. URL .. ":33882")
 
-		local function tostringAll(...)
-			local args = { ... }
-			for i, v in ipairs(args) do args[i] = tostring(v) end
-			return args
-		end
-
 		local function sendOutput(output_type, ...)
-			local t = tostringAll(...)
-			if connection and checkcaller() then connection:Send(Encode({ type = "output", output_type = output_type, output = table.concat(t, " ") })) end
+			local n = select("#", ...)
+			local args = { ... }
+			for i = 1, n do
+				args[i] = tostring(args[i])
+			end
+			local text = table.concat(args, "\t")
+			if connection and checkcaller() then connection:Send(Encode({ type = "output", output_type = output_type, output = text })) end
 		end
 
 		connection.OnMessage:Connect(function(msg)
